badbde12329bd8eedd63f1f58e880a2e25476c10
laja99 <layla.jaber.m@gmail.com>
2025-10-13 13:15:51 +0000

    update

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -11,5 +11,4 @@ g
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
-	return (0);
 }
