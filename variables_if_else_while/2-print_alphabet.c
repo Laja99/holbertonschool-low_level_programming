b556ac207896d971e7539b4a37a149f7f3556a14
laja99 <layla.jaber.m@gmail.com>
2025-10-15 17:23:14 +0000

    add

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main
+ *
+ * prints the alphabet in lowercase
+ *
+ * Return = Always 0 (success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
