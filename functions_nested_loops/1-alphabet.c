9d17fc8004f8d8e3c44050b24a4f09a7a42fa37c
laja99 <layla.jaber.m@gmail.com>
2025-10-17 03:44:52 +0000

    update

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Seccess)
+ * print_alphabet - Prints the lowercase alphabet followed by a new line
 */
 void print_alphabet(void)
 {
