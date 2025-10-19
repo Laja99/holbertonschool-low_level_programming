c969c4e18e5cfbb8e68314c77cc4b19f6138c482
laja99 <layla.jaber.m@gmail.com>
2025-10-19 01:06:29 +0000

    10 x alphabet

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - Prints the lowercase alphabet followed by a new line x10
+ * print_alphabet_x10 - Prints the alphabet followed by a new line x10
 */
 void print_alphabet_x10(void)
 {
 	char ch[] = "abcdefghijklmnopqrstuvwxyz";
 	int index;
 	int n;
-	
+
 	for (n = 0; n < 10; n++)
 	{
 		index = 0;
-		
 		while (ch[index] != '\0')
 		{
 			_putchar(ch[index]);
