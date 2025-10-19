675b803bd066950ac14dd6ff1006ff5a07570dfa
laja99 <layla.jaber.m@gmail.com>
2025-10-19 09:35:31 +0000

    add

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_last_digit - prints the last digit of a number
  * @n: the input integer
@@ -13,9 +14,9 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 		last_digit = -last_digit;
-	last_digit = n % 10;
 
 	_putchar(last_digit + '0');
 
 	return (last_digit);
+}
 }
