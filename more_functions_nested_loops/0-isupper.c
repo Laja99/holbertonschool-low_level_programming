ffc032a3c482e2cbb0aa246b0f23af2d67a53081
laja99 <layla.jaber.m@gmail.com>
2025-10-21 09:18:35 +0000

    upper case

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - if parameter c is upper case
+ * @c: the parameter
+ *
+ * Return: 1 if c is uppercase, 0 otherwise
+*/
+int _isupper(int c)
+{
+        char ch[] = "ABCDEFGHIJKLONOPQRSTUVWXYZ";
+        int index;
+
+        for (index = 0; ch[index] != '\0'; index++)
+        {
+                if (c == ch[index])
+                        return (1);
+        }
+        return (0);
+}
