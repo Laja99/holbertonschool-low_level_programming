63872d247a801861f439a4a31d73c52de9598ace
laja99 <layla.jaber.m@gmail.com>
2025-10-22 10:21:45 +0000

    add

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - print _ draws a straight line
+ *
+ * @n: the parameter of line
+ * Return
+*/
+void print_line(int n)
+{
+	int x;
+
+	x = 0;
+	while (x <= n)
+	{
+		_putchar('_');
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		x++;
+	}
+	return (0);
+}
