a7f31d125822c1e22211fc684a8802f12a7949e0
laja99 <layla.jaber.m@gmail.com>
2025-10-23 19:04:44 +0000

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - draws a triangle
+ *
+ * @size: the parameter
+ * Return
+*/
+void print_triangle(int size)
+{
+	int row, s, h;
+	int size = 5;
+
+	if (size <= 0)
+	{
+
+		_putchar('\n');
+		return (0);
+	}
+	for (row = 0; row < size; row++)
+	{
+		for (s = 0; s < size - row - 1; s++)
+		{
+			_putchar(' ');
+		}
+		for (h = 0; h <= row; h++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
