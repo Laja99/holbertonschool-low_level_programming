66d4c0b140c93a7b2a77ae6bc4d5134053065816
laja99 <layla.jaber.m@gmail.com>
2025-10-23 17:29:11 +0000

    FizzBuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - give the number from 1 to 100 with FizzBuzz
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if (c % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (c % 5 == 0 && c % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", c);
+		}
+	}
+	return (0);
+}
