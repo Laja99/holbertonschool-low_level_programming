2ca985333d2f67152b04c93eaf5730a62f1b00c1
laja99 <layla.jaber.m@gmail.com>
2025-10-25 12:30:33 +0000

    string to int

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -16,6 +16,10 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			if (result > (INT_MAX - (s[i] - '0')) / 10)
+			{
+				return (sign == 1 ? INT_MAX : INT_MIN);
+			}
 			result = result * 10 + (s[i] - '0');
 			started = 1;
 		}
