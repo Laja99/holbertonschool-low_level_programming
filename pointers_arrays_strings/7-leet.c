470757cdb881e7b6762152766d0a85c7177d147f
laja99 <layla.jaber.m@gmail.com>
2025-10-25 19:27:38 +0000

    m

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,7 +1,15 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ * @s: The string to encode
+ *
+ * Return: Pointer to the encoded string
+ */
 char *leet(char *s)
 {
 	char letters[] = "aAeEoOtTlL";
-	char numbers[] = "4433007711";
+	char codes[] = "4433007711";
 	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
@@ -10,7 +18,7 @@ char *leet(char *s)
 		{
 			if (s[i] == letters[j])
 			{
-				s[i] = numbers[j];
+				s[i] = codes[j];
 				break;
 			}
 		}
