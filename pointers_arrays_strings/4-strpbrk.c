90e5ad64c315225f97aec3140b7a7cac270774d4
laja99 <layla.jaber.m@gmail.com>
2025-10-29 10:27:06 +0000

    add

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -7,17 +7,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    unsigned int sa, ac;
-    
-    for (sa = 0; s[sa] != '\0'; sa++)
-    {
-        for (ac = 0; accept[ac] != '\0'; ac++)
-        {
-            if (s[sa] == accept[ac])
-            {
-             return (s + ac);
-            }
-        }
-    }
-    return (0);
+	unsigned int sa, ac;
+	
+	for (sa = 0; s[sa] != '\0'; sa++)
+	{
+		for (ac = 0; accept[ac] != '\0'; ac++)
+		{
+			if (s[sa] == accept[ac])
+			{
+				return (s + sa);
+			}
+		}
+	}
+	return (0);
 }
