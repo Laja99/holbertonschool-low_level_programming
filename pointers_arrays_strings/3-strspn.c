78ffa2f9980db0e78bebeb5d9ed408a43ee44830
laja99 <layla.jaber.m@gmail.com>
2025-10-29 08:06:34 +0000

    add

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -7,23 +7,22 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j, flag, count = 0;
+	unsigned int i, j, flag, count = 0;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        flag = 1;
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if (s[i] == accept[j])
-            {
-                count++;
-                flag = 0;
-                break;
-            }
-        }
-        if (flag == 1)
-            return (count);
-    }
-    return (count);
-
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		flag = 1;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+				flag = 0;
+				break;
+			}
+		}
+		if (flag == 1)
+			return (count);
+	}
+	return (count);
 }
