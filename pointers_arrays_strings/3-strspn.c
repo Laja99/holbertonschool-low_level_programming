6d6797d766bf549502b85544e1af1804502ea850
Layla Alshehri <layla.jaber.m@gmail.com>
2025-10-29 09:54:48 +0300

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,29 @@
 #include "main.h"
 /**
- * _strspn - gets the length of a perfix substring
+ * _strspn - get length of a prefix substring
  * @s: string to search
  * @accept: string to match
  * Return: number of bytes in that initial segment of s
  */
 unsigned int _strspn(char *s, char *accept)
-{}
+{
+    unsigned int i, j, flag, count = 0;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        flag = 1;
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                count++;
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+            return (count);
+    }
+    return (count);
+
+}
