51f836b0510dda0795cf5c611ab2e4520351b2d8
laja99 <layla.jaber.m@gmail.com>
2025-10-29 10:12:02 +0000

    add

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strpbrk - function locates the first occurrence in the string s of any of the bytes in the string accept
+ * @s: string to search
+ * @accept: string to match
+ * Return: a pointer to the byte in s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    unsigned int sa, ac;
+    
+    for (sa = 0; s[sa] != '\0'; sa++)
+    {
+        for (ac = 0; accept[ac] != '\0'; ac++)
+        {
+            if (s[sa] == accept[ac])
+            {
+             return (s + ac);
+            }
+        }
+    }
+    return (0);
+}
