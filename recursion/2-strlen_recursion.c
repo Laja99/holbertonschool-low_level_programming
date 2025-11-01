0bed5fb08c98b0d57886488bdcb05bef7b1e85d7
laja99 <layla.jaber.m@gmail.com>
2025-11-01 23:36:23 +0300

    recursion len

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - a function that returns the length of a string
+ * @s: array
+ */
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*S == '\0')
+	{
+		return;
+	}
+	_strlen_recursion(len + 1)
+	return (len);
+}
