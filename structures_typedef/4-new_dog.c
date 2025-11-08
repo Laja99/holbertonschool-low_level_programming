13ea9397094e69fbadb5d231989770b5e226128a
laja99 <layla.jaber.m@gmail.com>
2025-11-09 02:54:21 +0300

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,38 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <string.h>
+
+/**
+ * _strlen - returns length of string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: source
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -8,33 +40,37 @@
  * @age: dog's age
  * @owner: dog's owner
  *
- * Return: pointer to new dog, or NULL
+ * Return: pointer to new dog or NULL
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 	char *pN, *pO;
+	int lenN, lenO;
 
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
-	pN = malloc(strlen(name) + 1);
+	lenN = _strlen(name);
+	lenO = _strlen(owner);
+
+	pN = malloc(lenN + 1);
 	if (pN == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-	strcpy(pN, name);
+	_strcpy(pN, name);
 
-	pO = malloc(strlen(owner) + 1);
+	pO = malloc(lenO + 1);
 	if (pO == NULL)
 	{
 		free(pN);
 		free(d);
 		return (NULL);
 	}
-	strcpy(pO, owner);
+	_strcpy(pO, owner);
 
 	d->name = pN;
 	d->age = age;
