be4aac14b2629beec0821b2635e35c0f09f9cc46
laja99 <layla.jaber.m@gmail.com>
2025-11-09 02:14:13 +0300

    init_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - a dog's basic info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: This struct is used to store basic information about dogs.
+ */
 struct dog
 {
 	char *name;
