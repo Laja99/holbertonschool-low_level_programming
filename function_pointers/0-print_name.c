5a583923cf10013249c574ba64d525a69d273e90
laja99 <layla.jaber.m@gmail.com>
2025-11-10 12:45:29 +0300

    create files and print mane function

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* print_name - prints a name using a given function
+* @name: the name to be printed
+* @f: pointer to the function that prints the name
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
+	else if (name == NULL)
+	{
+		printf("name is NULL\n");
+	}
+	else if (f == NULL)
+	{
+		printf("there is no function\n");
+	}
+
+}
