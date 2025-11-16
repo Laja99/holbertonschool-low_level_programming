3d53d6915ca0a573bffb351b02c1fde3cb719189
laja99 <layla.jaber.m@gmail.com>
2025-11-16 21:01:37 +0300

    opcode

---
+++ function_pointers/100-main_opcodes.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Error\n");
+        exit(1);
+    }
+    int n = atoi(argv[1]);
+    if (n < 0)
+    {
+        printf("Error\n");
+        exit(2);
+    }
+    unsigned char *size = (unsigned char *) main;
+    for (int i = 0; i < n; i++)
+    {
+        printf("%02x", size[i]);
+        if (i == n - 1)
+            printf("\n");
+        else
+            printf(" "); 
+    }
+    return (0);
+}
