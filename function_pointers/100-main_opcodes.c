56489476bbdd075083542ec9bff51549dc3453fb
laja99 <layla.jaber.m@gmail.com>
2025-11-16 21:03:21 +0300

    for gnu89

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[])
         exit(2);
     }
     unsigned char *size = (unsigned char *) main;
-    for (int i = 0; i < n; i++)
+    int i;
+    for ( i = 0; i < n; i++)
     {
         printf("%02x", size[i]);
         if (i == n - 1)
