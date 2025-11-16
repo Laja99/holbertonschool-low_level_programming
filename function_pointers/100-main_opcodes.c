216cd618ef412643ece308163b16652b76010d5e
laja99 <layla.jaber.m@gmail.com>
2025-11-16 22:33:28 +0300

    opcode

--- function_pointers/100-main_opcodes.c
+++ function_pointers/100-main_opcodes.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success
+ */
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Error\n");
-        exit(1);
-    }
-    int n = atoi(argv[1]);
-    if (n < 0)
-    {
-        printf("Error\n");
-        exit(2);
-    }
-    unsigned char *size = (unsigned char *) main;
-    int i;
-    for ( i = 0; i < n; i++)
-    {
-        printf("%02x", size[i]);
-        if (i == n - 1)
-            printf("\n");
-        else
-            printf(" "); 
-    }
-    return (0);
+	int n, i;
+	unsigned char *size;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	n = atoi(argv[1]);
+
+	if (n < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	size = (unsigned char *)main;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%02x", size[i]);
+		if (i == n - 1)
+			printf("\n");
+		else
+			printf(" ");
+	}
+
+	return (0);
 }
