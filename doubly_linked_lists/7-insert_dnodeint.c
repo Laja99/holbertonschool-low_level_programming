a00ca5e5c1d0adab1a966b1d448b21d6225e3026
laja99 <layla.jaber.m@gmail.com>
2025-11-30 11:52:01 +0300

    Kernel style code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,37 +1,44 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: pointer to the head of the list
+ * @idx: index where the new node should be added (starts at 0)
+ * @n: value to store in the new node
+ * Return: address of the new node, or NULL if it failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    unsigned int i;
-    dlistint_t *new_node, *current;
+	dlistint_t *new_node, *temp;
+	unsigned int i;
 
-    if (!h)
-        return (NULL);
+	if (h == NULL)
+		return (NULL);
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    current = *h;
-    for (i = 0; current && i < idx - 1; i++)
-        current = current->next;
+	temp = *h;
+	for (i = 0; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
 
-    if (!current)
-        return (NULL);
+	if (temp == NULL)
+		return (NULL);
 
-    if (!current->next)
-        return (add_dnodeint_end(h, n));
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
 
-    new_node = malloc(sizeof(dlistint_t));
-    if (!new_node)
-        return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-    new_node->n = n;
-    new_node->prev = current;
-    new_node->next = current->next;
-    current->next->prev = new_node;
-    current->next = new_node;
+	new_node->n = n;
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	temp->next->prev = new_node;
+	temp->next = new_node;
 
-    return (new_node);
+	return (new_node);
 }
 
