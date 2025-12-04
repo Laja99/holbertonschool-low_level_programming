746e291b07108aa090d56c53ddc0f5c3cbab6403
Layla_Alshehri <layla.jaber.m@gmail.com>
2025-12-04 13:16:27 +0300

    Refactor cp function for improved error handling and code clarity

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,51 +1,103 @@
 #include "main.h"
+/**
+ * cp - copy contents from file_from to file_to
+ * @file_from: source filename
+ * @file_to: destination filename
+ *
+ * Return:
+ *   0   on success
+ *   98  if can't read from file_from
+ *   99  if can't write to file_to
+ *  >=100 encoded close error as 100 + fd_value
+ */
 int cp(const char *file_from, const char *file_to)
 {
-    int fd_from, fd_to, r_bytes, w_bytes;
-    char buffer[1024];
+	int fd_from, fd_to;
+	ssize_t r_bytes, w_bytes;
+	char buffer[1024];
 
-    if (file_from == NULL || file_to == NULL)
-        return (-1);
-    fd_from = open(file_from, O_RDONLY);
-    if (fd_from == -1)
-        return (-1);
-    fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
-    if (fd_to == -1)
-    {
-        close(fd_from);
-        return (-1);
-    }
-    while ((r_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
-    {
-        w_bytes = write(fd_to, buffer, r_bytes);
-        if (w_bytes == -1 || w_bytes != r_bytes)
-        {
-            close(fd_from);
-            close(fd_to);
-            return (-1);
-        }
-    }
-    if (r_bytes == -1)
-    {
-        close(fd_from);
-        close(fd_to);
-        return (-1);
-    }
-    close(fd_from);
-    close(fd_to);
-    return (0);
+	if (file_from == NULL || file_to == NULL)
+		return (98);
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+		return (98);
+
+	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		if (close(fd_from) == -1)
+			return (100 + fd_from);
+		return (99);
+	}
+
+	while ((r_bytes = read(fd_from, buffer, sizeof(buffer))) > 0)
+	{
+		ssize_t total = 0;
+
+		while (total < r_bytes)
+		{
+			w_bytes = write(fd_to, buffer + total, r_bytes - total);
+			if (w_bytes == -1)
+			{
+				if (close(fd_from) == -1)
+					return (100 + fd_from);
+				if (close(fd_to) == -1)
+					return (100 + fd_to);
+				return (99);
+			}
+			total += w_bytes;
+		}
+	}
+	if (r_bytes == -1)
+	{
+		if (close(fd_from) == -1)
+			return (100 + fd_from);
+		if (close(fd_to) == -1)
+			return (100 + fd_to);
+		return (98);
+	}
+	if (close(fd_from) == -1)
+		return (100 + fd_from);
+	if (close(fd_to) == -1)
+		return (100 + fd_to);
+	return (0);
 }
+/**
+ * main - entry point for cp program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * All error messages and exit codes are handled here.
+ */
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-        return (97);
-    }
-    if (cp(argv[1], argv[2]) == -1)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't copy from %s to %s\n", argv[1], argv[2]);
-        return (98);
-    }
-    return (0);
+	int res;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	res = cp(argv[1], argv[2]);
+
+	if (res == 0)
+		return (0);
+	if (res == 98)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (res == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (res >= 100)
+	{
+		int fd = res - 100;
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	exit(100);
 }
