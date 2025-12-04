47945a4023e549bf4f58f7939e8955a06d7b301b
Layla_Alshehri <layla.jaber.m@gmail.com>
2025-12-04 10:16:43 +0300

    Refactor append_text_to_file function for consistent formatting

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,30 +1,30 @@
 #include "main.h"
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: The name of the file.
- * @text_content: The NULL terminated string to add to the end of the file.
- * Return: If the function fails or filename is NULL - -1.
- */
+* append_text_to_file - Appends text at the end of a file.
+* @filename: The name of the file.
+* @text_content: The NULL terminated string to add to the end of the file.
+* Return: If the function fails or filename is NULL - -1.
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, len = 0, xwrite;
+	int fd, len = 0, xwrite;
 
-    if (filename == NULL)
-        return (-1);
-    if (text_content != NULL)
-    {
-        while (text_content[len])
-            len++;
-    }
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
-    xwrite = write(fd, text_content, len);
-    if (xwrite == -1 || xwrite != len)
-    {
-        close(fd);
-        return (-1);
-    }
-    close(fd);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+		len++;
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	xwrite = write(fd, text_content, len);
+	if (xwrite == -1 || xwrite != len)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+	return (1);
 }
